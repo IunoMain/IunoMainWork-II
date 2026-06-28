ad6aa6448fe56e00415887ee469e73a036816b74
IunoMain <iunomainlove@gmail.com>
2026-06-27 22:54:52 -0600

    Java

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main (String[] args) {
+        
+        int edad = 25;
+        double precio = 6.66;
+        boolean activo = true;
+        char letra = 'I';
+        String nombre = "Iuno";
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Activo: " + activo);
+        System.out.println("Letra: " + letra);
+    }
+}
